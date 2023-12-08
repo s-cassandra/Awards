0d01e7bec56e35ace0ed76324ba26b61b78ec471
s-cassandra <152437473+s-cassandra@users.noreply.github.com>
2023-12-08 21:20:27 +0000

    awards.py

--- awards.py
+++ awards.py
@@ -1,5 +1,21 @@
+'''
+TASK INFORMATION: 
+- Design a program that determines the award a person competing in a triathlon will receive. 
+- Your program should read in the times (in minutes) for all three events of a triathlon, namely 
+  swimming, cycling, and running, and then calculate and display the total time taken to complete the triathlon.
+- The award a participant receives is based on the total time taken to complete the triathlon. 
+  The qualifying time for awards is 100 minutes. Display the award that the participant will receive based on the following criteria:
+
+
+Qualifying Criteria:                                    Time Range:          Award:
+Within the qualifying time                              0 - 100 minutes      Provincial Colours
+Within 5 minutes of the qualifying time                 101 - 105 minutes    Provincial Half Colours
+Within 10 minutes of the qualifying time                106 - 110 minutes    Provincial Scroll 
+More than 10 minutes off from the qualifying time       111+ minutes         No award 
+'''
 
 '''
+
 Start
 1. have user input minutes for the following variables (and turn into integers): swimming_minutes, cycling_minutes, and running_minutes
 2. create variable called triathlon_minutes with value being sum of all events
